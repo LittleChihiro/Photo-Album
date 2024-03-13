f3ed1bd4a9d387dbff81c5dc906f49c293a75a68
ChihirO <jasmine.moradiafkan@gmail.com>
2024-03-13 17:12:28 +0100

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -121,11 +121,11 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'static',
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
