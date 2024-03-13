04ed6aa42e734b31486924db32713531659cabd8
ChihirO <jasmine.moradiafkan@gmail.com>
2024-03-13 11:18:14 +0100

    update

--- photos/urls.py
+++ photos/urls.py
@@ -2,7 +2,7 @@ import profile
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
-from .views import home, gallery, viewPhoto, addPhoto, loginUser, signup, profile, edit_profile
+from .views import home, gallery, viewPhoto, addPhoto, loginUser, profile, edit_profile
 
 urlpatterns = [
     path('', home, name='home'),  # Home-Seite als die Standardseite
@@ -10,7 +10,6 @@ urlpatterns = [
     path('photo/<str:pk>', viewPhoto, name='photo'),
     path('add/', addPhoto, name='add'),
     path('login/', loginUser, name='login'),
-    path('signup/', signup, name='signup'),
     path('profile/', profile, name='profile'),
     path('edit_profile/', edit_profile, name='edit_profile'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
--- photos/views.py
+++ photos/views.py
@@ -122,18 +122,6 @@ def loginUser(request):
     return render(request, 'photos/login.html')
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'photos/signup.html', {'form': form})
-
 def profile(request):
     return render(request, 'photos/profile.html')
 
